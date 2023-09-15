f8bc00ac17ed45a0f10486a7c3c552e64ddfc0f3
Sergio Saavedra <sergio.saavedrajr@gmail.com>
2023-09-15 18:03:26 -0300

    Custom HomeScreen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,12 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Mi Manual del Bebe</Text>
     </View>
   );
 };
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    // justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 20,
+  }
+})
 
 export default HomeScreen;
